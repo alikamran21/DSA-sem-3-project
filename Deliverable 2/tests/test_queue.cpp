4c9b3c9a40f823275b9aaa2a6976206eded381ff
Fatima-RM <u2024177@giki.edu.pk>
2025-10-19 00:14:37 +0500

    testqueue updated

--- Deliverable 2/tests/test_queue.cpp
+++ Deliverable 2/tests/test_queue.cpp
@@ -1,19 +1,36 @@
- #include "../include/queue_monitor.h"
+#include "../include/queue_monitor.h"
 #include "../include/user_action.h"
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main() {
     QueueMonitor q;
-    UserAction a1("2025-10-18 12:00", "U101", "Login", "System", "5s", "OpenFile", "Success");
-    UserAction a2("2025-10-18 12:10", "U102", "Logout", "System", "3s", "-", "Success");
+    time_t now = time(nullptr);
 
+    // UserAction(userID, action, processName, duration (double), timestamp (time_t), nextAction, status)
+    UserAction a1("U101", "Login", "System", 5.0, now - 180, "OpenFile", "Success");
+    UserAction a2("U102", "Logout", "System", 3.0, now, "-", "Success");
+    UserAction a3("U103", "Process_Start", "TaskMgr", 1.5, now + 60, "Monitor", "Running");
+
+
+    cout << "\n--- Enqueueing Actions ---\n";
     q.enqueueAction(a1);
     q.enqueueAction(a2);
+    q.enqueueAction(a3);
 
+    cout << "\n--- Queue Status ---\n";
+    cout << "Queue Size: " << q.getSize() << endl;
+    q.peekFront();
+
+    cout << "\n--- Dequeueing Actions ---\n";
+    q.dequeueAction();
     q.peekFront();
     q.dequeueAction();
     q.dequeueAction();
+
+    cout << "\n--- Empty Queue Test ---\n";
     q.dequeueAction();  // test empty queue
 
     return 0;
