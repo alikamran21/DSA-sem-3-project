1f5b14a57988f9925462087cf072db688def40a7
Fatima-RM <u2024177@giki.edu.pk>
2025-10-19 00:17:16 +0500

    teststack updated

--- Deliverable 2/tests/test_stack.cpp
+++ Deliverable 2/tests/test_stack.cpp
@@ -1,19 +1,37 @@
 #include "../include/stack_monitor.h"
 #include "../include/user_action.h"
 #include <iostream>
+#include <ctime>
+
 using namespace std;
 
 int main() {
     StackMonitor stack;
-    UserAction a1("2025-10-18 14:00", "U201", "Open", "Notepad", "2s", "Save", "Running");
-    UserAction a2("2025-10-18 14:05", "U202", "Close", "Notepad", "1s", "-", "Done");
+    time_t now = time(nullptr);
 
+    UserAction a1("U201", "Open", "Notepad", 2.0, now - 300, "Save", "Running");
+    UserAction a2("U202", "Close", "Notepad", 1.0, now, "-", "Done");
+    UserAction a3("U203", "Error", "Driver", 0.5, now + 60, "Restart", "Failure");
+
+
+    cout << "\n--- Pushing Actions ---\n";
     stack.pushAction(a1);
     stack.pushAction(a2);
+    stack.pushAction(a3);
 
-    stack.peekTop();
+    cout << "\n--- Stack Status ---\n";
+    // FIX: Method name changed from peekTop() to peekAction()
+    stack.peekAction();
+
+    cout << "\n--- Display All Actions ---\n";
+    stack.displayAll();
+
+    cout << "\n--- Popping Actions ---\n";
     stack.popAction();
     stack.popAction();
+    stack.popAction();
+
+    cout << "\n--- Empty Stack Test ---\n";
     stack.popAction(); // Empty pop test
 
     return 0;
