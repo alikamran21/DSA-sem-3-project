170848b5c81432997109ea6376a81cc132e316d1
Fatima-RM <u2024177@giki.edu.pk>
2025-10-18 23:54:05 +0500

    Updated FileIO

--- Deliverable 2/include/file_io.h
+++ Deliverable 2/include/file_io.h
@@ -8,6 +8,7 @@
 
 using namespace std;
 
+// Node structure for the basic singly linked list used internally by FileIO.
 struct Node {
     UserAction data;
     Node* next;
@@ -17,12 +18,15 @@ struct Node {
 // Handles saving and reading user actions from text files
 class FileIO {
 public:
+    // Saves a single action entry (unformatted, appended)
     static bool saveAction(const UserAction& action, const string& filename);
+    // Reads and displays file contents to the console
     static void readFile(const string& filename);
+    // Saves a linked list of actions as a formatted, sorted table
     static bool saveActionsToFile(Node* head, const string& filename);
 };
 
-// Linked list utility
+// Linked list utility: adds a new action node to the end of the list
 void appendNode(Node*& head, const UserAction& action);
 
 #endif
