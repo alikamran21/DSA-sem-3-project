220ccf4f6f87b11f5a4d807596c1c7b207ef0599
Ali Kamran <alikamran1223@gmail.com>
2025-11-18 11:45:50 +0500

    added hash_profile.cpp

--- Deliverable 3/hash_profiles.h
+++ Deliverable 3/hash_profiles.h
@@ -1,5 +1,5 @@
-#ifndef HASH_PROFILE_H
-#define HASH_PROFILE_H
+#ifndef HASH_PROFILES_H
+#define HASH_PROFILES_H
 
 #include <string>
 #include <iostream>
@@ -12,31 +12,26 @@ class hashNode
 
 public:
     string userId;
-    avlProfile profile;
-    hashNode* next;
-    hashNode(const string &id) : userId(id) , next(nullptr) {}
+    avlProfile *profilePtr; // to be fixed on avl file being added
+    hashNode *next;
+    hashNode(const string &id, avlProfile &ptr) : userId(id), profilePtr(ptr), next(nullptr) {} // to be fixed on avl file being added
 };
 
 class hashTable
 {
 private:
-    hashTable** table;
+    hashTable **table;
     int capacity;
-    int hashFunction (const string &key) const;
+    int hashFunction(const string &key) const;
+
 public:
     hashTable(int size = 20);
     ~hashTable();
+    
+    void addProfile(const string &userId, AvlProfile *profilePtr);
+    AvlProfile *getProfile(const string &userId); // to be corrected on avlfile being made
 
-    void registerUser (const string& userId);
-    // AvlProfile* getProfile(const string& userId);  to be corrected on avlfile being made
-
-    void updateProfile(const string& userId, const string command , double duration);
-
-    void matchProfile(const string& userId, const string command, double duration );
-
-    void displayTable();
-
+    void displayTable() const;
 };
-
 
 #endif
